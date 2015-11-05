bd2fd135436fc01ea01664f99181097855ce8ac2
lebarsfa <fabrice.le_bars@ensta-bretagne.fr>
2015-11-05 17:13:09 +0100

    Added MDMRNG2KML_clampToGround and updated LocalBoxes2KML

---
+++ MDMRNG2KML_clampToGround/MDMRNG2KML_Debug.bat
@@ -0,0 +1,2 @@
+cd /d "%~dp0"
+start Debug\MDMRNG2KML.exe
--- MDMRNG2KML_clampToGround/Main.c
+++ MDMRNG2KML_clampToGround/Main.c
@@ -7,35 +7,7 @@
 #	undef _MSC_VER
 #endif // defined(__GNUC__) || defined(__BORLANDC__)
 
-#include "Env.h"
-	
-// Parameters.
-unsigned int skipfactor = 0;
-
-inline int LoadConfig()
-{
-	FILE* file = NULL;
-	char line[256];
-
-	memset(line, 0, sizeof(line));
-
-	// Default values.
-	skipfactor = 10;
-
-	file = fopen("LocalBoxes2KML.txt", "r");
-	if (file != NULL)
-	{
-		if (fgets3(file, line, sizeof(line)) == NULL) printf("Invalid configuration file.\n");
-		if (sscanf(line, "%u", &skipfactor) != 1) printf("Invalid configuration file.\n");
-		if (fclose(file) != EXIT_SUCCESS) printf("fclose() failed.\n");
-	}
-	else
-	{
-		printf("Configuration file not found.\n");
-	}
-
-	return EXIT_SUCCESS;
-}
+#include "OSMisc.h"
 
 int main(int argc, char* argv[])
 {
@@ -45,21 +17,19 @@ int main(int argc, char* argv[])
 	FILE* fileout = NULL;
 	char szTemp[256];
 	char szName[256];
-	double d0 = 0, d1 = 0, d2 = 0, d3 = 0, d4 = 0, d5 = 0, d6 = 0, d7 = 0, d8 = 0, d9 = 0, d10 = 0; 
-	double dt = 0, t0 = 0, t = 0, latmin = 0, longmin = 0, latmax = 0, longmax = 0, altitude = 0;
-	double xmin = 0, ymin = 0, xmax = 0, ymax = 0, z = 0;
 	char line[4096];
 	unsigned int i = 0;
+	double t = 0, latitude = 0, longitude = 0, latitude2 = 0, longitude2 = 0, distance = 0, t_raw = 0;
 	time_t tt;
 	struct tm* timeptr = NULL;
 	char timebuf[32];
 
 	if (argc != 2)
 	{
-		strcpy(szFileInPath, "logstate.csv");
+		strcpy(szFileInPath, "mdmrng.csv");
 		printf("Warning : No parameter specified.\n");
-		printf("Usage : LocalBoxes2KML file.csv.\n");
-		printf("Default : LocalBoxes2KML %.255s.\n", szFileInPath);
+		printf("Usage : MDMRNG2KML file.csv.\n");
+		printf("Default : MDMRNG2KML %.255s.\n", szFileInPath);
 	}
 	else
 	{
@@ -76,9 +46,6 @@ int main(int argc, char* argv[])
 	printf("Control Panel\\Regional and Language Options\\Customize\\Numbers\n\n");
 	printf("if you do not know if you should use a '.' or a ',' in floating points numbers\n\n");
 	printf("or a ';' or ',' in list separators.\n\n");
-
-	LoadConfig();
-	LoadEnv();
 
 	filein = fopen(szFileInPath, "r");
 	if (filein == NULL)
@@ -102,56 +69,30 @@ int main(int argc, char* argv[])
 #endif // _DEBUG
 		return EXIT_FAILURE;
 	}
-
-	strcpy(timebuf, "0");
-
+	
 	fprintf(fileout, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
 	fprintf(fileout, "<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\" xmlns:kml=\"http://www.opengis.net/kml/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n");
 	fprintf(fileout, "<Document>\n");
-	fprintf(fileout, "\t<name>logstate</name>\n\t<open>1</open>\n\t<visibility>1</visibility>\n");
-	fprintf(fileout, "\t<Style id=\"sn_poly\">\n\t\t<PolyStyle>\n\t\t\t<fill>0</fill>\n\t\t\t<color>ff0000ff</color>\n\t\t\t<width>2</width>\n\t\t</PolyStyle>\n\t</Style>\n");
+	fprintf(fileout, "<name>%.249s</name>\n", szName);
+	fprintf(fileout, "\t<Style id=\"sn_ylw-pushpin\">\n\t\t<LineStyle>\n\t\t\t<color>ff0000ff</color>\n\t\t\t<width>2</width>\n\t\t</LineStyle>\n\t</Style>\n");
 
 	printf("Converting...\n");
 
-	t = 0;
 	i = 0;
 	memset(line, 0, sizeof(line));
 	while (fgets3(filein, line, sizeof(line)) != NULL) 
 	{
-		t0 = t;
-		if (sscanf(line, "%lf;%lf;%lf;%lf;%lf;%lf;%lf;%lf;%lf;%lf;%lf;%lf;"
-			"%lf;%lf;%lf;%lf", 
-			&t, &d0, &d1, &d2, &d3, &d4, &d5, &d6, &d7, &d8, &d9, &d10, 
-			&xmin, &xmax, &ymin, &ymax) == 16) 
+		if (sscanf(line, "%lf;%lf;%lf;%lf;%lf;%lf;%lf", 
+			&t, &latitude, &longitude, &latitude2, &longitude2, &distance, &t_raw) == 7) 
 		{
-			dt = t-t0;
-			if ((i%skipfactor) == skipfactor-1)
-			{
-				tt = (time_t)t;
-				timeptr = gmtime(&tt);
-				if (timeptr != NULL) strftime(timebuf, 32, "%Y-%m-%dT%H:%M:%SZ", timeptr);
-
-				EnvCoordSystem2GPS(lat_env, long_env, alt_env, angle_env, xmin, ymin, z, &latmin, &longmin, &altitude);
-				EnvCoordSystem2GPS(lat_env, long_env, alt_env, angle_env, xmax, ymax, z, &latmax, &longmax, &altitude);
-
-				fprintf(fileout, "\t<Placemark>\n\t\t<visibility>0</visibility>\n\t\t<TimeStamp>\n\t\t\t<when>%.32s</when>\n\t\t</TimeStamp>\n\t\t<name>%d</name>\n\t\t<styleUrl>#sn_poly</styleUrl>\n\t\t<Polygon>\n\t\t\t<altitudeMode>clampToGround</altitudeMode>\n"
-					"\t\t\t<tessellate>1</tessellate>\n\t\t\t<outerBoundaryIs>\n\t\t\t\t<LinearRing>\n", timebuf, i);
-
-				fprintf(fileout, "\t\t\t\t\t<coordinates>"
-					"%f,%f,%f "
-					"%f,%f,%f "
-					"%f,%f,%f "
-					"%f,%f,%f "
-					"%f,%f,%f "
-					"</coordinates>\n", 
-					longmin, latmin, altitude, 
-					longmin, latmax, altitude, 
-					longmax, latmax, altitude, 
-					longmax, latmin, altitude, 
-					longmin, latmin, altitude);
-
-				fprintf(fileout, "\t\t\t\t</LinearRing>\n\t\t\t</outerBoundaryIs>\n\t\t</Polygon>\n\t</Placemark>\n");
-			}
+			tt = (time_t)t;
+			timeptr = gmtime(&tt);
+			if (timeptr != NULL) strftime(timebuf, 32, "%Y-%m-%dT%H:%M:%SZ", timeptr);
+			fprintf(fileout, "\t<Placemark>\n\t\t<name>t=%.2f,d=%.1f</name>\n", t_raw, distance);
+			fprintf(fileout, "\t\t<TimeStamp>\n\t\t\t<when>%.32s</when>\n\t\t</TimeStamp>\n", timebuf);
+			fprintf(fileout, "\t\t<styleUrl>#sn_ylw-pushpin</styleUrl>\n\t\t<LineString>\n\t\t\t<tessellate>1</tessellate>\n\t\t\t<altitudeMode>clampToGround</altitudeMode>\n\t\t\t<coordinates>\n");
+			fprintf(fileout, "\t\t\t\t%f,%f,%f %f,%f,%f\n", longitude2, latitude2, 0.0, longitude, latitude, 0.0);
+			fprintf(fileout, "\t\t\t</coordinates>\n\t\t</LineString>\n\t</Placemark>\n");
 			i++;
 		}
 		else
@@ -163,7 +104,7 @@ int main(int argc, char* argv[])
 
 	fprintf(fileout, "</Document>\n</kml>\n");
 
-	printf("Found %u positions.\n", i);
+	printf("Found %u rng.\n", i);
 
 	if (fclose(fileout) != EXIT_SUCCESS) 
 	{
